3977ec51e5fb59ed171c5afa7b80451b9636c5e8
Maxwell Tsu <maxtsu@juniper.net>
2025-02-01 19:36:45 +0000

    updated py

--- kafka-client.py
+++ kafka-client.py
@@ -1,70 +1,59 @@
-#!/usr/bin/env python
-import json
+#!/usr/bin/python3
 import signal
-import time
+import yaml
 
 from concurrent.futures import ThreadPoolExecutor
-
 from kafka import KafkaConsumer
 
 
+## Global
+kill_signal = False
+number_consumers = 4
+yaml_file = "kafka-config.yaml"
+
+with open(yaml_file, 'r') as f:
+    config = yaml.load(f, Loader=yaml.SafeLoader)
+if "number.consumers" in config:
+    number_consumers = config["number.consumers"]
+
+brokers = config["bootstrap.servers"].split(',')
+
+#SIGINT to terminate the program
+def handler_stop_signals(signum, frame):
+    print("Received SIGINT signal")
+    global kill_signal
+    kill_signal = True
+signal.signal(signal.SIGINT, handler_stop_signals)
+
 #consumer function
-def consumer():
+def consumer(id):
+    print("Consumer {} Starting kafka".format(id))
+    global kill_signal
     run = True
     while run:
         try:
             #Connect to Kafka broker and topic
-            consumer = KafkaConsumer(topics, bootstrap_servers=[brokers], sasl_plain_username=user, sasl_plain_password=password, security_protocol=secprotocol, sasl_mechanism=saslmech)
-            # iterate through kafka messages
+            consumer = KafkaConsumer(config["topics"], bootstrap_servers=brokers, \
+                        sasl_plain_username=config["sasl.username"], sasl_plain_password=config["sasl.password"], \
+                        security_protocol=config["security.protocol"], sasl_mechanism=config["sasl.mechanisms"], \
+                        ssl_cafile=config["ssl.ca.location"], auto_offset_reset = config["auto.offset.reset"])
+            print("Connected to brokers")
             for msg in consumer:
-                print('Received kafka message {}'.format(msg))
-                # Load Kafa message to Json
                 telemetry_msg = msg.value
-                print('Processing kafka message {}'.format(str(telemetry_msg)[:300]))
-                ### Telemetry Data message in JSON format
-                telemetry_msg_json = json.loads(telemetry_msg)
-                ### Extract source, path, port, key_value and timestamp from message
-                ### ... key_values are a dictionary of {folder,{key1:value1,key2:value2},etc.} extracted from path of message
-                ### e.g. 'interfaces/interface[name=ge-0/0/1]/state' interface name ge-0/0/1 is in folder 'interface'
-                content_source, sensor_path_message, port, key_values, timestamp = getSource(telemetry_msg_json)
-                ### Create name-path string for matching configuration tuple
-                messageNamePath = content_source + '-' + sensor_path_message
-
-                ### Search configuration for this name-path pair
-                ### Check tuple inside tuple
-                ### If there is no match to configuration tuples. Message is ignored
-                matching_rule = 'Null'
-                for item in configuration:
-                    if messageNamePath in item:
-                        matching_rule = item # Matching item from config.json
-                        matchInRulesJSON = False # Flag for a match in the rules.json file
-                        #Find details for the rule in the rules.JSON
-                        for rule in rulesJSON: #iterate through all the rules in rulesJSON
-                            # Match on the rule-id between config.json & rules.json
-                            if matching_rule[6] == rule['rule-id']:
-                                matchInRulesJSON = True # Set flag to true
-                                logging.info('Process rule {}'.format(rule['rule-id']))
-                                fields =dataPointsFromMessage(telemetry_msg_json, rule)
-                                # Extract index_values list from rules.json tuple
-                                for index in rule['index_values']: #iterate across all index values
-                                    # extract the index values from the message path string
-                                    value = kvs_dict_parse(key_values, index['path'] , index['index'])
-                                    fields.update({index['index']:value})
-                                if not 'Null' in fields: # As long as there is no 'Null' in fields write to TSDB
-                                    # Take data and write to TSDB create thread for TSDB write. Call function with DB fields
-                                    # Thread limit with semaphore
-                                    thread = threading.Thread(target=writeTSDB, args=(tand_host, tand_port, matching_rule, timestamp, fields, semaphore))
-                                    thread.start()
-                            else:
-                                continue # continue with next iteration for a match
-                        if matchInRulesJSON == False: #no path match for telemetry message
-                            #does not match any rule
-                            logging.info('Message no matching rule in rules.json {}'.format(str(telemetry_msg_json)[:300]))
-                if matching_rule == 'Null':
-                    # log ignored message
-                    logging.info('Ignore kafka message {}'.format(str(telemetry_msg)[:300]))
-                if not run:  # Break with SIGHUP
-                    logging.info('Received SIGHUP breaking from kafka messaging loop')
+                print("{}".format(str(telemetry_msg)))
+                if kill_signal:
+                    run = False
+                    print("consumer {} received termination".format(id))
                     break
         except Exception as e:
-            logging.critical('Error when connecting with kafka consumer: ' + str(e))
+            print("Consumer {} Error when connecting with kafka consumer: {}".format(id, str(e)))
+        finally:
+            print("Unsubscribe and close kafka consumer {}".format(id))
+            consumer.unsubscribe()
+            consumer.close()
+    run = False
+
+with ThreadPoolExecutor(max_workers=number_consumers) as executor:
+        executor.map(consumer, range(number_consumers))
+
+print("Main thread completed")
