8b11e53db3de0a9efa693381707dae2a42260bac
Maxwell Tsu <maxtsu@juniper.net>
2025-04-23 16:32:54 +0100

    f

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -39,12 +39,12 @@ func ReadFile(fileName string) []byte {
 func NewTLSConfig(clientCertFile, clientKeyFile, caCertFile string) (*tls.Config, error) {
 	tlsConfig := tls.Config{}
 
-	// Load client cert
-	cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
-	if err != nil {
-		return &tlsConfig, err
-	}
-	tlsConfig.Certificates = []tls.Certificate{cert}
+	// // Load client cert
+	// cert, err := tls.LoadX509KeyPair(clientCertFile, clientKeyFile)
+	// if err != nil {
+	// 	return &tlsConfig, err
+	// }
+	// tlsConfig.Certificates = []tls.Certificate{cert}
 
 	// Load CA cert
 	//caCert, err := ioutil.ReadFile(caCertFile)
