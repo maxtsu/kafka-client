5cf34ec963dad3ceaedd0c4ac13d87ac857f82a8
Maxwell Tsu <maxtsu@juniper.net>
2026-02-06 14:36:36 +0000

    query code

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module influxclient
 
 go 1.22
+
+require (
+	github.com/gologme/log v1.3.0
+	github.com/influxdata/influxdb1-client v0.0.0-20220302092344-a9ab5670611c
+)
--- main.go
+++ main.go
@@ -6,15 +6,28 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/gologme/log"
+	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 // Version 0.9
 const config_file = "kafka-config.yaml"
 
+var InfluxClient client.Client // Influx client
+var tand_host = "172.16.16.21"
+var tand_port = "28086"
+
 func main() {
 	fmt.Println("influx application v0.1")
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Set logging level
+	level := 5 //debug level
+	log.EnableLevelsByNumber(level)
+	log.EnableFormattedPrefix()
+	log.Infoln("Logging configured Set at level ", level)
 
 	// // Rad the config file
 	// byteResult := ReadFile(config_file)
@@ -25,6 +38,55 @@ func main() {
 	// }
 	// fmt.Printf("kafka-config.yaml: %+v\n", configYaml)
 
+	//Create InfluxDB client
+	InfluxCreateClient(tand_host, tand_port)
+	log.Infof("Influx client create %+v\n", InfluxClient)
+
+	// Build query
+	db := "hb-default:my-group01:192.168.100.100"
+	measurement := "cpu"
+	q := fmt.Sprintf(`
+        SELECT mean("usage_system") AS usage_system
+        FROM %q
+        WHERE time >= now() - 15m AND "host" = 'server-01'
+        GROUP BY time(1m) fill(null)
+    `, measurement)
+
+	// Execute
+	res, err := queryDB(InfluxClient, client.NewQuery(q, db, "ns")) // precision: "ns","u","ms","s","m","h"
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\n%+v\n", res)
+
+}
+
+func queryDB(cl client.Client, q client.Query) ([]client.Result, error) {
+	resp, err := cl.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Error() != nil {
+		return nil, resp.Error()
+	}
+	return resp.Results, nil
+}
+
+// Create InfluxDB client Global variable InfluxClient
+func InfluxCreateClient(tand_host string, tand_port string) {
+	// Make Influx client
+	url := "http://" + tand_host + ":" + tand_port
+	var err error
+	InfluxClient, err = client.NewHTTPClient(client.HTTPConfig{
+		Addr: url,
+	})
+	if err != nil {
+		log.Errorln("Error creating InfluxDB Client: ", err.Error())
+	}
+	defer InfluxClient.Close()
+	log.Infoln("InfluxDB Client connection", InfluxClient)
+	//return influxClient
 }
 
 // Function to read text file return byteResult
